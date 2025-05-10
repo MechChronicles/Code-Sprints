8f8a628f3d9f3da9e0883895ffe000963bf52248
Sammaina-alt <samuel.gachuru24@students.dkut.ac.ke>
2025-05-10 12:48:30 +0300

    Update programmer_day.py

--- problems/easy/programmer_day/programmer_day.py
+++ problems/easy/programmer_day/programmer_day.py
@@ -1,4 +1,12 @@
 
 def dayOfProgrammer(year):
-    # Write your code here
+    def dayOfProgrammer(year):
+    if year == 1918:
+        return "26.09.1918"
+    elif (1700 <= year <= 1917) and year % 4 == 0:
+        return f"12.09.{year}"
+    elif (year >= 1919) and (year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)):
+        return f"12.09.{year}"
+    else:
+        return f"13.09.{year}"
     return "12.09." + str(year)
