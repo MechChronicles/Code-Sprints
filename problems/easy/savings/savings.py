7b6dbdc31c40bd32e8547d54bcccbb3e719e32cd
Brian kamotho <brianwakonyo8@gmail.com>
2025-05-10 03:04:55 -0700

    Update savings.py

--- problems/easy/savings/savings.py
+++ problems/easy/savings/savings.py
@@ -1,3 +1,8 @@
-def count_savings( num):
-    # write your code here
+def count_savings( num)
+    return n * (n + 1) // 2  # Integer division
+
+# Example usage
+n = int(input("Enter number of days: "))
+print("Total savings after", n, "days is:", total_savings(n))
+
     return 0
