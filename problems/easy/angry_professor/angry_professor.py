b81d96d75f9b40313eab1fef435961a3d7e2d8b2
MechChronicles <brianongoro2030@gmail.com>
2025-05-10 13:18:34 +0300

    Update angry_professor.py

--- problems/easy/angry_professor/angry_professor.py
+++ problems/easy/angry_professor/angry_professor.py
@@ -1,4 +1,9 @@
-
 def angryProfessor(k, a):
-    # Write your code here
-    return "YES"
+    # Count the number of students who arrived on time or early
+    on_time_students = sum(1 for arrival in a if arrival <= 0)
+
+    # If the number of on-time students is less than the threshold, cancel class
+    if on_time_students < k:
+        return "YES"  # Class is cancelled
+    else:
+        return "NO"   # Class is not cancelled
